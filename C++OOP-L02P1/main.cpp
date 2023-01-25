b835c0c7106e30ddc4e3f74d50a0f0c543222723
Tadeusz <tadeusz.potoniec@parhelium.com>
2023-01-25 11:15:52 +0100

    Initial Commit

---
+++ C++OOP-L02P1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++OOP-L02P1
+//
+//  Created by Tadeusz Potoniec on 25/01/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
